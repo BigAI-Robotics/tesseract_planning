3d0257bfcf62b510a4f087b6c21e681f855e673a
Yida Niu <niuyida@bigai.ai>
2022-09-27 10:45:08 +0800

    feat: added ik solution fail output

--- tesseract_motion_planners/3mo/src/3mo_utils.cpp
+++ tesseract_motion_planners/3mo/src/3mo_utils.cpp
@@ -295,6 +295,7 @@ filterCollisionIK(tesseract_environment::Environment::ConstPtr env,
   std::vector<std::pair<Eigen::VectorXd, double>> result;
   // check collision
   tesseract_collision::ContactResultMap contact_result;
+  tesseract_collision::ContactResultMap best_contact_result;
   tesseract_collision::DiscreteContactManager::Ptr contact_manager = env->getDiscreteContactManager()->clone();
   size_t best_collision_count = 1000;
   std::pair<Eigen::VectorXd, double> best_solution = ik_input.at(0);
@@ -312,6 +313,7 @@ filterCollisionIK(tesseract_environment::Environment::ConstPtr env,
     {
       best_collision_count = contact_result.size();
       best_solution = ik;
+      best_contact_result = contact_result;
       if (contact_result.size() == 0)
       {
         result.push_back(ik);
@@ -321,7 +323,12 @@ filterCollisionIK(tesseract_environment::Environment::ConstPtr env,
 
   if (result.empty())
   {
-    CONSOLE_BRIDGE_logWarn("ik solution is empty, saving best solution into result");
+    CONSOLE_BRIDGE_logWarn("ik solution is empty, saving best solution into result. best collision count: %d",
+                           best_collision_count);
+    for (auto& collision : best_contact_result)
+    {
+      std::cout << "\t" << collision.first.first << " -->|<-- " << collision.first.second << std::endl;
+    }
     result.push_back(best_solution);
   }
   CONSOLE_BRIDGE_logDebug("collision free ik: %ld/%ld", ik_input.size(), result.size());
